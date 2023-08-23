ace45991600d1d351baf315b5f22e75c0d7ea058
mjunaidshaikh128 <115004219+mjunaidshaikh128@users.noreply.github.com>
2023-08-23 08:28:53 +0500

    login validation added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +13,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  markAllControlsAsTouched() {
+    Object.keys(this.loginForm.controls).forEach((controlName) => {
+      this.loginForm.controls[controlName].markAsTouched();
+    });
+  }
+
+  login() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value);
+
+    }
+    this.markAllControlsAsTouched();
+  }
+
 
 }
